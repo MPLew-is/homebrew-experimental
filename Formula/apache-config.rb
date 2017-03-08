1df1ba5deca1e90f52f9394e85390b54151cc4ed
Mike Lewis <mike@mplew.is>
2017-03-08 01:23:00 -0500

    Update to version 0.1.1

--- Formula/apache-config.rb
+++ Formula/apache-config.rb
@@ -1,8 +1,8 @@
 class ApacheConfig < Formula
 	desc "Script for managing Apache configuration like a2enmod and related scripts on Debian"
 	homepage "https://github.com/MPLew-is/apache-config"
-	url "https://github.com/MPLew-is/apache-config/archive/v0.1.0.tar.gz"
-	sha256 "8d087bd49594071bc07cb38ff10a53ce125d4d362065d0ead609acf00f987680"
+	url "https://github.com/MPLew-is/apache-config/archive/v0.1.1.tar.gz"
+	sha256 "71f178d6f3d4f6a55a1eea7993e8d54202eb6507fa2834d44e21b752f9194ea"
 	head "https://github.com/MPLewis/apache-config.git"
 	
 	bottle :unneeded
