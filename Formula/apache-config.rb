03a8eb590a77044f27954eea90e7c0cd59655313
Mike Lewis <mike@mplew.is>
2017-03-08 00:50:02 -0500

    Update hash
    Original was placeholder value

--- Formula/apache-config.rb
+++ Formula/apache-config.rb
@@ -2,7 +2,7 @@ class ApacheConfig < Formula
 	desc "Script for managing Apache configuration like a2enmod and related scripts on Debian"
 	homepage "https://github.com/MPLew-is/apache-config"
 	url "https://github.com/MPLew-is/apache-config/archive/v0.1.0.tar.gz"
-	sha256 "cb66fecd3986a935b0169dc326615b7d475d1fe4da14542ca912e30b59b978e7"
+	sha256 "8d087bd49594071bc07cb38ff10a53ce125d4d362065d0ead609acf00f987680"
 	head "https://github.com/MPLewis/apache-config.git"
 	
 	bottle :unneeded
