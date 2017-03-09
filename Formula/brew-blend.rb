6065f0347eaa73f828814b6ba17400cf31c9f6e3
Mike Lewis <mike@mplew.is>
2017-03-09 12:55:31 -0500

    Add URL and hash for initial brew-blend release

--- Formula/brew-blend.rb
+++ Formula/brew-blend.rb
@@ -1,6 +1,8 @@
 class BrewBlend < Formula
 	desc "Meta-formulae for homebrew"
 	homepage "https://github.com/MPLew-is/homebrew-blend"
+	url "https://github.com/MPLew-is/homebrew-blend/archive/v0.1.0.tar.gz"
+	sha256 "52af4c6f0af4fb81fcf5bd8d2e6841af2ea149f4ac8a57ce5c77a96aea1f3c63"
 	head "https://github.com/MPLew-is/homebrew-blend.git"
 	
 	bottle :unneeded
