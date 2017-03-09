a39c63859a87784712d1266eedbdc319b1399093
Mike Lewis <mike@mplew.is>
2017-03-09 07:39:32 -0500

    Add brew-blend formula

---
+++ Formula/brew-blend.rb
@@ -0,0 +1,20 @@
+class ApacheConfig < Formula
+	desc "Meta-formulae for homebrew"
+	homepage "https://github.com/MPLew-is/homebrew-blend"
+	head "https://github.com/MPLewis/homebrew-blend.git"
+	
+	bottle :unneeded
+	
+	depends_on "jq"
+	
+	def install
+		bin.install "brew-blend.sh" => "brew-blend"
+	end
+	
+	def caveats; <<-EOS.undent
+		To finish installation, please run:
+			brew-blend install --self
+		
+		EOS
+	end
+end
