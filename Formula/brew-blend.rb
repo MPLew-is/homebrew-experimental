d883debae6fe21cab59021a007accb84d979c456
homebrew-experimental (CircleCI) <mike@mplew.is>
2017-03-09 19:10:19 +0000

    Upgrade 'brew-blend' to 'v0.1.1-5-g308c4d9' (CircleCI auto-deploy)

--- Formula/brew-blend.rb
+++ Formula/brew-blend.rb
@@ -1,8 +1,8 @@
 class BrewBlend < Formula
 	desc "Meta-formulae for homebrew"
 	homepage "https://github.com/MPLew-is/homebrew-blend"
-	url "https://github.com/MPLew-is/homebrew-blend/archive/v0.1.0.tar.gz"
-	sha256 "52af4c6f0af4fb81fcf5bd8d2e6841af2ea149f4ac8a57ce5c77a96aea1f3c63"
+	url "https://github.com/MPLew-is/homebrew-blend/archive/v0.1.1-5-g308c4d9.tar.gz"
+	sha256 "65cb0d83afe4d3ce6b8a8ae6aeff5b1dfe2965f6f0eb1920d00d009fe62c89ec"
 	head "https://github.com/MPLew-is/homebrew-blend.git"
 	
 	bottle :unneeded
