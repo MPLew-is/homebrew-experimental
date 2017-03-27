46c0b390a64aa413c7f3a110f441c60415f80dd7
Mike Lewis <mike@mplew.is>
2017-03-27 14:39:00 -0400

    Use subtree branch in formula

--- Formula/git-subtree.rb
+++ Formula/git-subtree.rb
@@ -1,7 +1,7 @@
 class GitSubtree < Formula
 	desc "Updated git-subtree that includes a '--no-commit' option (to allow use while upstream patch is being processed)"
 	homepage "https://github.com/MPLew-is/git"
-	head "https://github.com/MPLew-is/git.git"
+	head "https://github.com/MPLew-is/git.git", :branch => "subtree-no-commit"
 	
 	bottle :unneeded
 	
